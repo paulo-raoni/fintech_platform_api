1d6adb89010f43081a565b0b49b398dbbcb12bd4
Paulo Raoni <raoni_jkd@hotmail.com>
2021-03-27 00:04:54 -0300

    Refactoring on auth files.

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
-const controller = require("../app/controllers/authController");
 const User = require("../app/models/user");
 const {
   registerValidators,
   validateFields,
 } = require("../app/validators/authValidator");
-
 const {
   authRequired,
   authenticateAfterLogin,
@@ -17,13 +15,13 @@ router.get("/home", authRequired, (req, res) => {
 });
 
 router.get("/login", authRequired, (req, res) => {
-  if(req.user) {
+  if (req.user) {
     req.logout(); //logout to prevent login after register
   }
   res.status(200).send({ message: "You are at LOGIN PAGE!" });
 });
 
-router.get("/register", authRequired, (req, res)=>{
+router.get("/register", authRequired,  (req, res) => {
   res.status(200).send({ response: "You are at REGISTER!" });
 });
 
@@ -31,29 +29,34 @@ router.post(
   "/register",
   registerValidators(),
   (req, res, next) => {
-    const { body } = req;
-    const user = new User({
-      username: body.username,
-      password: body.password,
-      cpf: body.cpf,
-    });
-
-    // Validator using express-validator
-    const errorList = validateFields(req);
-
-    if (errorList.length) {
-      res.status(400).send({ errorList });
-    } else {
-      user.save((err) => {
-        if(err) {
-          console.log("There was an error on user register!");
-          res.status(401).send({ error: "There was an error on user register!" });
-        } else {
-          next();
-        }
+    try {
+      const { body } = req;
+      const user = new User({
+        username: body.username,
+        password: body.password,
+        cpf: body.cpf,
       });
-    }
 
+      // Validator using express-validator
+      const errorList = validateFields(req);
+
+      if (errorList.length) {
+        res.status(400).send({ errorList });
+      } else {
+        user.save((err) => {
+          if (err) {
+            console.log("There was an error on user register!");
+            res
+              .status(401)
+              .send({ error: "There was an error on user register!" });
+          } else {
+            next();
+          }
+        });
+      }
+    } catch (error) {
+      res.status(400).send(error);
+    }
   },
   authenticateAfterRegister
 );
@@ -61,8 +64,12 @@ router.post(
 router.post("/login", authenticateAfterLogin);
 
 router.get("/logOut", (req, res) => {
-  req.logout();
-  res.status(200).send({ message: "Logged Out successfully!" });
+  try {
+    req.logout();
+    res.status(200).send({ message: "Logged Out successfully!" });
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
